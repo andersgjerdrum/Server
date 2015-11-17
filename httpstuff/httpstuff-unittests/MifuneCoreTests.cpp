4de9a9d7149df529a0b7fadfcc69a4b6c517076c
Anders Gjerdrum <angjerd@microsoft.com>
2015-11-17 16:03:38 +0100

    Added unit test for httpstructs

---
+++ httpstuff/httpstuff-unittests/MifuneCoreTests.cpp
@@ -0,0 +1,44 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "..\httpstuff\httpstructs.h"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+using namespace MifuneCore;
+namespace httpstuffunittests
+{		
+	TEST_CLASS(MifuneCoreTests)
+	{
+	public:
+		
+		TEST_METHOD(httpstructs)
+		{
+			httprequest req(R"foo(GET /tutorials/other/top-20-mysql-best-practices/ HTTP/1.1
+Host: net.tutsplus.com
+User-Agent: Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.5) Gecko/20091102 Firefox/3.5.5 (.NET CLR 3.5.30729)
+Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8
+Accept-Language: en-us,en;q=0.5
+Accept-Encoding: gzip,deflate
+Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.7
+Keep-Alive: 300
+Connection: keep-alive
+Cookie: PHPSESSID=r2t5uvjq435r4q7ib3vtdjq120
+Pragma: no-cache
+Cache-Control: no-cache
+X-Forwarded-For: 192.168.10.1
+)foo");
+
+			Assert::IsTrue(req.RawHeaderFields[HttpHeaderField::HostFieldName].compare("net.tutsplus.com") == 0,L"HostFieldName should be equal");
+			Assert::IsTrue(req.RawHeaderFields[HttpHeaderField::UserAgentFieldName].compare("Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.5) Gecko/20091102 Firefox/3.5.5 (.NET CLR 3.5.30729)") == 0,L"UserAgentFieldName should be equal");
+			Assert::IsTrue(req.RawHeaderFields[HttpHeaderField::AcceptFieldName].compare("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8") == 0, L"AcceptFieldName should be equal");
+			Assert::IsTrue(req.RawHeaderFields[HttpHeaderField::AcceptLanguageFieldName].compare("en-us,en;q=0.5") == 0, L"AcceptLanguageFieldName should be equal");
+			Assert::IsTrue(req.RawHeaderFields[HttpHeaderField::AcceptEncodingFieldName].compare("gzip,deflate") == 0, L"AcceptEncodingFieldName should be equal");
+			Assert::IsTrue(req.RawHeaderFields[HttpHeaderField::AcceptCharsetFieldName].compare("ISO-8859-1,utf-8;q=0.7,*;q=0.7") == 0, L"AcceptCharsetFieldName should be equal");
+			Assert::IsTrue(req.RawHeaderFields[HttpHeaderField::KeepAliveFieldName].compare("300") == 0, L"KeepAliveFieldName should be equal");
+			Assert::IsTrue(req.RawHeaderFields[HttpHeaderField::ConnectionFieldName].compare("keep-alive") == 0, L"ConnectionFieldName should be equal");
+			Assert::IsTrue(req.RawHeaderFields[HttpHeaderField::CookieFieldName].compare("PHPSESSID=r2t5uvjq435r4q7ib3vtdjq120") == 0, L"CookieFieldName should be equal");
+			Assert::IsTrue(req.RawHeaderFields[HttpHeaderField::PragmaFieldName].compare("no-cache") == 0, L"PragmaFieldName should be equal");
+			Assert::IsTrue(req.RawHeaderFields[HttpHeaderField::CacheControlFieldName].compare("no-cache") == 0, L"CacheControlFieldName should be equal");
+			Assert::IsTrue(req.RawHeaderFields[HttpHeaderField::XForwardForFieldName].compare("192.168.10.1") == 0, L"XForwardForFieldName should be equal");
+		}
+	};
+}
--- httpstuff/httpstuff/httpstructs.h
+++ httpstuff/httpstuff/httpstructs.h
@@ -2,7 +2,7 @@
 
 #include <string>
 #include <sstream>
-#include <list>
+#include <map>
 using namespace std;
 namespace MifuneCore
 {
@@ -29,6 +29,7 @@ namespace MifuneCore
 		 const string LastModifiedFieldName("Last-Modified");
 		 const string XPingbackFieldName("X-Pingback");
 		 const string VaryFieldName("Vary");
+		 const string XForwardForFieldName("X-Forwarded-For");
 
 
 	};
@@ -40,10 +41,10 @@ namespace MifuneCore
 		void parse(string buffer) 
 		{
 			std::stringstream stream(buffer);
-			std::list<string> vector;
 			string pch;
 			std::getline(stream, pch);
 			Get = pch;
+
 			while (std::getline(stream, pch))
 			{
 				PutField(pch);
@@ -56,96 +57,12 @@ namespace MifuneCore
 			string Key;
 			getline(stream, Key,':');
 			string Value;
-			getline(stream, Value, ':');
-			if (Key.compare(HttpHeaderField::UserAgentFieldName)) {
-				UserAgent = Value;
-			}
-			else if (Key.compare(HttpHeaderField::HostFieldName)) {
-				Host = Value;
-			}
-			else if (Key.compare(HttpHeaderField::AcceptFieldName)) {
-				Accept = Value;
-			}
-			else if (Key.compare(HttpHeaderField::AcceptLanguageFieldName)) {
-				AcceptLanguage = Value;
-			}
-			else if (Key.compare(HttpHeaderField::AcceptEncodingFieldName)) {
-				AcceptEncoding = Value;
-			}
-			else if (Key.compare(HttpHeaderField::AcceptCharsetFieldName)) {
-				AcceptCharset = Value;
-			}
-			else if (Key.compare(HttpHeaderField::KeepAliveFieldName)) {
-				KeepAlive = Value;
-			}
-			else if (Key.compare(HttpHeaderField::ConnectionFieldName)) {
-				Connection = Value;
-			}
-			else if (Key.compare(HttpHeaderField::CookieFieldName)) {
-				Cookie = Value;
-			}
-			else if (Key.compare(HttpHeaderField::PragmaFieldName)) {
-				Pragma = Value;
-			}
-			else if (Key.compare(HttpHeaderField::CacheControlFieldName)) {
-				CacheControl = Value;
-			}
-			else if (Key.compare(HttpHeaderField::TransferEncodingFieldName)) {
-				TransferEncoding = Value;
-			}
-			else if (Key.compare(HttpHeaderField::DateFieldName)) {
-				Date = Value;
-			}
-			else if (Key.compare(HttpHeaderField::ServerFieldName)) {
-				Server = Value;
-			}
-			else if (Key.compare(HttpHeaderField::XPoweredByFieldName)) {
-				XPoweredBy = Value;
-			}
-			else if (Key.compare(HttpHeaderField::ExpiresFieldName)) {
-				Expires = Value;
-			}
-			else if (Key.compare(HttpHeaderField::EtagFieldName)) {
-				Etag = Value;
-			}
-			else if (Key.compare(HttpHeaderField::ContentTypeFieldName)) {
-				ContentType = Value;
-			}
-			else if (Key.compare(HttpHeaderField::LastModifiedFieldName)) {
-				LastModified = Value;
-			}
-			else if (Key.compare(HttpHeaderField::XPingbackFieldName)) {
-				XPingback = Value;
-			}
-			else if (Key.compare(HttpHeaderField::VaryFieldName)) {
-				Vary = Value;
-			}
-
-
+			getline(stream, Value);
+			RawHeaderFields.insert(std::pair<string,string>(Key, Value.substr(Value.find_first_not_of(' '), Value.length())));
 		}
+
 	public:
-		string Raw;
-		string UserAgent;
-		string Host;
-		string Accept;
-		string AcceptLanguage;
-		string AcceptEncoding;
-		string AcceptCharset;
-		string KeepAlive;
-		string Connection;
-		string Cookie;
-		string Pragma;
-		string CacheControl;
-		string TransferEncoding;
-		string Date;
-		string Server;
-		string XPoweredBy;
-		string Expires;
-		string Etag;
-		string ContentType;
-		string LastModified;
-		string XPingback;
-		string Vary;
+		std::map<string, string> RawHeaderFields;
 		httprequest(string buffer) :Raw(buffer)
 		{
 			parse(buffer);
